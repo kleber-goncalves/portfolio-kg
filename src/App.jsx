564e1696a31cd3b016754450571709670268861f
Kleber Lucas.G.S. <lenovomicrosof1@gmail.com>
2026-09-14 10:47:27 -0300

    feat: adicionar modo de experiência ao componente Competencia e otimizar animações no Card1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 // ScrollSmooth - Lenis
 import SmoothScroll from "./components/SmoothScroll";
 
+// React
+import { useState } from "react";
+
 // Layout
 import Competencia from "./layout/Competencia";
 import ProjetoT from "./layout/ProjetosTeste2";
@@ -11,7 +14,6 @@ import Footer from "./layout/Footer";
 // Styles
 import "./App.css";
 
-
 // Components
 import HeroSectionTransition from "./components/HeroSectionTransition";
 import ButtonReset from "./components/buttonReset";
@@ -20,22 +22,32 @@ import CursorTrail from "./components/CursorTrail";
 // Reduzir animações
 import ExperienceSelector from "./components/ExperienceSelector";
 
+// Experience Mode
+import { getExperienceMode } from "./utils/experienceMode";
+
 function App() {
+    const [experienceMode] = useState(() => getExperienceMode() || "full");
+
     return (
         <>
             <CursorTrail />
 
             <SmoothScroll>
                 <ExperienceSelector />
+
                 <div className="relative z-[1] w-full">
                     <ButtonReset />
 
-                    <HeroSectionTransition />
+                    <HeroSectionTransition experienceMode={experienceMode} />
 
-                    <Competencia />
+                    <Competencia experienceMode={experienceMode} />
+
                     <ProjetoT />
+
                     <Diferenciais />
+
                     <Formacao />
+
                     <Footer />
                 </div>
             </SmoothScroll>
